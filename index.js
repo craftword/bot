fdf034778d25bbd1460d940fb3db838c8706032f
Abimbola Olaitan <craftwordltd@gmail.com>
2017-08-01 03:20:56 +0100

    fix a bug on index.js

--- index.js
+++ index.js
@@ -20,46 +20,6 @@ app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
-app.get('/webhook', function (req, res) {
-	if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
-		res.status(200).send(req.query['hub.challenge'])
-	}
-	res.status(403).send('Error, wrong token')
-})
-
-// handler receiving messages
-app.post('/webhook', function (req, res) {  
-    var events = req.body.entry[0].messaging;
-    for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
-    }
-    res.status(200).end();
-});
-
-
-
-// generic function sending messages
-const sendMessage = (recipientId, message) => {  
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
 
 // Spin up the server
 app.listen(app.get('port'), function() {
